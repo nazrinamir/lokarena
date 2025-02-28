d25d87ce34070ee889e10a7a992587ae68a80c35
MSI\nazri <nazrinamirrullah@gmail.com>
2025-02-28 22:09:07 +0800

    Add React Router and initial routing structure
    - Integrated react-router-dom v7.2.0
    - Configured basic routing in App.tsx with protected routes
    - Removed default Create React App boilerplate
    - Updated package.json and package-lock.json dependencies
    - Removed unnecessary files like logo.svg, reportWebVitals, and setupTests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './pages/Dashboard.tsx';
+import Matchmaking from './pages/Matchmaking.tsx';
+import NotFound from './pages/NotFound.tsx';
+import Register from './pages/AuthenticationPages/Register.tsx';
 import './App.css';
+import ProtectedRoute from './contexts/ProtectedRoute.tsx';
+import { AuthProvider } from './contexts/AuthContext.tsx';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path='/' />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+            <Route path="/matchmaking" element={<ProtectedRoute><Matchmaking /></ProtectedRoute>} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
 export default App;
+
